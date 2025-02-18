63ef8426c0a6599806ff84862c3a376ebe4ced30
PauVentura30 <pauventuraa@gmail.com>
2025-02-18 02:04:49 +0000

    final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+
+	const [nuevoTodo, setNuevoTodo] = useState("Tarea Nueva");
+	const [todos, setTodos] = useState(["una","y dos tareas de prueba"]);
+
+	const handleClick = () => {
+		console.log("Nueva Tarea", nuevoTodo)
+		setTodos({...todos, nuevoTodo})
+	}
+
+	const deleteTodo = (indice) => {
+		//todos los elementos menos el que tenga el indice que recibo
+		const listaNueva = todos.filter((todo, i) => i !== indice)
+		setTodos(listaNueva)
+	}
+
+	const handleChange = (event) => {
+		setNuevoTodo(event.target.value);
+	}
+
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">
+				To-do List React Fetch
+			</h1>
+			<div className="container">
+				<div className="d-flex gap-2">
+					<input type="text" className="form-control" onChange={handleChange} />
+					<button onClick={handleClick} className="btn btn-primary">
+						Agregar tarea
+					</button>
+				</div>
+			</div>
+			<p>Nueva tarea: {nuevoTodo}</p>
+			<ul className="list-group">
+				{todos.map((todo, indice) => {
+					return(
+						<li className="list-group-item d-flex justify-content-between align-items-center">
+							{todo} <button className="btn btn-danger" onClick={() => deleteTodo(indice)}>Borrar</button>
+						</li>
+					)
+				})}
+			</ul>
 		</div>
 	);
 };
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "../styles/index,css";
+
+import Home from "./components/Home.jsx";
+
+ReactDOM.render(<Home />, document.querySelector("#app"));
